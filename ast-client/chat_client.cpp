8d9b5ffdea592a1b6ec8d9bb200d25060750e1fe
Lukas Borges <lborg019@fiu.edu>
2017-07-15 23:36:09 -0400

    now creating sockets with shared pointers, moved send/recv part to separate function

--- ast-client/chat_client.cpp
+++ ast-client/chat_client.cpp
@@ -19,8 +19,109 @@
 
 using boost::asio::ip::tcp;
 
-enum { max_length = 1024 };
 enum { buffer_size = 8 };
+
+std::string check_file(std::string message, size_t request_length, std::shared_ptr<boost::asio::ip::tcp::socket> s)
+{
+	boost::asio::streambuf receive_buffer;
+	boost::system::error_code error;
+	char rcv_buf[buffer_size];	// 8 byte buffer for message size
+	std::string response;		// store server response here
+
+	//send message size as an 8 byte message:
+	boost::asio::write(*s, boost::asio::buffer(std::to_string(request_length), buffer_size));
+	std::cout << "waiting to receive ack...\n";
+
+	// receive ack
+	size_t len = s->read_some(boost::asio::buffer(rcv_buf), error);
+	std::string received_ack(rcv_buf[0], sizeof(rcv_buf));
+	received_ack.resize(len);
+	if (error && error != boost::asio::error::eof) {
+		std::cout << "error status: " << error.message() << std::endl;
+	}
+	else
+	{
+		std::string reply(rcv_buf);
+		reply = reply.substr(0, 3);
+		std::cout << reply << ": server ackd\n";
+	}
+
+	// send message content
+	boost::algorithm::trim_left(message);
+	boost::asio::write(*s, boost::asio::buffer(message, message.length()));
+
+	// receive message size:
+	int remaining_data = 0;
+	rcv_buf[0] = '\0';
+	len = s->read_some(boost::asio::buffer(rcv_buf), error);
+	std::string received_size(rcv_buf[0], sizeof(rcv_buf));
+	received_size.resize(len);
+	if (error && error != boost::asio::error::eof) {
+		std::cout << "error status: " << error.message() << std::endl;
+	}
+	else {
+		std::string reply(rcv_buf);
+		std::string s = boost::regex_replace(
+			reply,
+			boost::regex("[^0-9]*([0-9]+).*"),
+			std::string("\\1")
+		);
+		std::cout << "receiving: " << s << " bytes\n";
+
+		std::string::size_type sz;
+		remaining_data = std::stoi(s, &sz); // save it as integer
+	}
+
+	// receive message content:
+	rcv_buf[0] = '\0'; // clean buffer
+	if (remaining_data > 260) // big message, write to file:
+	{
+		// format file name
+		// ../files/file1.py
+		std::cout << "reached receive file loop\n";
+	}
+	else // receive regular echo message:
+	{
+		std::cout << "receiving regular message\n";
+		while (remaining_data != 0)
+		{
+			if (remaining_data >= buffer_size)
+			{
+				// receive slab 8 byte or bigger
+				len = s->read_some(boost::asio::buffer(rcv_buf, buffer_size), error);
+				if (error && error != boost::asio::error::eof) {
+					std::cout << "error status: " << error.message() << std::endl;
+				}
+				else {
+					std::string reply(rcv_buf);
+					reply = reply.substr(0, len);
+					response.append(reply);
+					std::cout << "wrote: " << len << " bytes\n";
+					remaining_data = remaining_data - len;
+				}
+			}
+			else // receive slab smaller than 8 bytes
+			{
+				// read entire buffer, shrink correct size afterwards
+				len = s->read_some(boost::asio::buffer(rcv_buf, buffer_size), error);
+				if (error && error != boost::asio::error::eof) {
+					std::cout << "error status: " << error.message() << std::endl;
+				}
+				else
+				{
+					std::string reply(rcv_buf);
+					reply = reply.substr(0, remaining_data - 1);
+					std::cout << "wrote: " << len << " bytes\n";
+					response.append(reply);
+					remaining_data = remaining_data - len;
+					//clean buffer maybe?
+				}
+			}
+		}
+	}
+	std::cout << "response: " << response << "\n";
+	return response;
+}
 
 int main(int argc, char* argv[])
 {
@@ -32,121 +133,35 @@ int main(int argc, char* argv[])
 			return 1;
 		}
 
-		boost::asio::streambuf receive_buffer;
 		boost::asio::io_service io_service;
-		boost::system::error_code error;
+		//tcp::resolver resolver(io_service);
+		//boost::asio::connect(s, resolver.resolve({ argv[1], argv[2] }));
 
-		tcp::socket s(io_service);
-		tcp::resolver resolver(io_service);
-		boost::asio::connect(s, resolver.resolve({ argv[1], argv[2] }));
+		// create a shared ptr so we can pass the socket to a function:
+		std::shared_ptr<boost::asio::ip::tcp::socket> current_socket;
+		current_socket = std::make_shared<boost::asio::ip::tcp::socket>(io_service);
 
-		std::cout << "Enter message: ";
-		std::string message;
-		std::string response;
-		char rcv_buf[buffer_size];	// 8 byte buffer for message size
+		// pass command line arguments instead of hard-coding:
+		boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::address::from_string("127.0.0.1"), 5555);
+		boost::system::error_code ec;
+		current_socket->connect(endpoint, ec);
 
-		getline(std::cin, message); // store user input in memory
-		message.append("\r");		// apend message end
-		size_t request_length = message.length(); // calculate the size of the message
+		std::string message;		// store client input here
+		std::string response;		// store server response here
 
-		//send message size as an 8 byte message:
-		boost::asio::write(s, boost::asio::buffer(std::to_string(request_length), buffer_size));
-		std::cout << "waiting to receive ack...\n";
-
-		// receive ack
-		size_t len = s.read_some(boost::asio::buffer(rcv_buf), error);
-		std::string received_ack(rcv_buf[0], sizeof(rcv_buf));
-		received_ack.resize(len);
-		if (error && error != boost::asio::error::eof) {
-			std::cout << "error status: " << error.message() << std::endl;
-		}
-		else {
-			std::string reply(rcv_buf);
-			reply = reply.substr(0, 3);
-			std::cout << reply << ": server ackd\n";
-		}
-
-		// send message content
-		boost::algorithm::trim_left(message);
-		boost::asio::write(s, boost::asio::buffer(message, message.length()));
-
-		// receive message size:
-		int remaining_data = 0;
-		//rcv_buf[0] = {0}; // clean buffer
-		rcv_buf[0] = '\0';
-		len = s.read_some(boost::asio::buffer(rcv_buf), error);
-		std::string received_size(rcv_buf[0], sizeof(rcv_buf));
-		received_size.resize(len);
-		if (error && error != boost::asio::error::eof) {
-			std::cout << "error status: " << error.message() << std::endl;
-		}
-		else {
-			std::string reply(rcv_buf);
-			std::string s = boost::regex_replace(
-				reply,
-				boost::regex("[^0-9]*([0-9]+).*"),
-				std::string("\\1")
-			);
-			std::cout << "receiving: " << s << " bytes\n";
-
-			std::string::size_type sz;
-			remaining_data = std::stoi(s, &sz); // save it as integer
-		}
-
-		// receive message content:
-		rcv_buf[0] = '\0'; // clean buffer
-
-		if (remaining_data > 260) // big message, write to file:
+		for (;;)
 		{
-			// format file name
-			// ../files/file1.py
-			std::cout << "reached receive file loop\n";
+			std::cout << "Enter message: ";
+			getline(std::cin, message);
+			message.append("\r");		// append message end to the message
+			size_t request_length = message.length(); // calculate the size of the message
+			response = check_file(message, request_length, current_socket); // empty string if something goes wrong
 		}
-		else // receive regular echo message:
-		{
-			std::cout << "receiving regular message\n";
-			while (remaining_data != 0)
-			{
-				if (remaining_data >= buffer_size)
-				{
-					// receive slab 8 byte or bigger
-					len = s.read_some(boost::asio::buffer(rcv_buf, buffer_size), error);
-					if (error && error != boost::asio::error::eof) {
-						std::cout << "error status: " << error.message() << std::endl;
-					}
-					else {
-						std::string reply(rcv_buf);
-						reply = reply.substr(0, len);
-						response.append(reply);
-						std::cout << "wrote: " << len << " bytes\n";
-						remaining_data = remaining_data - len;
-					}
-				}
-				else // receive slab smaller than 8 bytes
-				{
-					// read entire buffer, shrink correct size afterwards
-					len = s.read_some(boost::asio::buffer(rcv_buf, buffer_size), error);
-					if (error && error != boost::asio::error::eof) {
-						std::cout << "error status: " << error.message() << std::endl;
-					}
-					else
-					{
-						std::string reply(rcv_buf);
-						reply = reply.substr(0, remaining_data-1);
-						std::cout << "wrote: " << len << " bytes\n";
-						response.append(reply);
-						remaining_data = remaining_data - len;
-						//clean buffer maybe?
-					}
-				}
-			}
-		}
-		std::cout << "response: " << response << "\n";
 	}
 	catch (std::exception& e)
 	{
 		std::cerr << "Exception: " << e.what() << "\n";
 	}
-	system("PAUSE");
+	//system("PAUSE");
 	return 0;
 }
